6c2f6588af67b842c4643d6b6b599f365657fad6
urtesala <urtesaladziute@gmail.com>
2022-10-27 10:15:56 +0300

    map, filter

---
+++ js/filter.js
@@ -0,0 +1,48 @@
+"use strict";
+console.log("filter.js");
+
+// array.filter(function(currentValue, index, arr) {})
+// const atrkinktasMasyvas = array.filter((currentValue, index, arr) => {})
+// i atrkinktasMasyvas gryzta reisme tada kai vidine funkcija grazina true
+
+// Array.prototype.filter - skirtas atrinkti reiksmems pagal salyga.
+// kai atrinktame masyve yra maziau reiksmiu ne is ko rinkom
+// dazniausiai nenaudojamas pakeisti reiksmem
+
+const nums = [1, -5, 2, 10, -4, 3];
+const elements = [1, "true", "true", false, null, undefined, "blue", 50];
+
+// atrinkti teigiamus
+const positivesArr = nums.filter((sk) => {
+  if (sk > 0) {
+    return true;
+  } else {
+    return false;
+  }
+  // return sk > 0;
+});
+//                          sk  sk  sk sk
+const positivesArrOneLine = [1, -5, 2, 10, -4, 3].filter((sk) => sk > 0);
+const negativeArrOneLine = nums.filter((sk) => sk < 0);
+
+console.log("positivesArrOneLine ===", positivesArrOneLine);
+console.log("negativeArrOneLine ===", negativeArrOneLine);
+
+const more1less10 = (sk) => sk > 1 && sk < 10;
+const even = (sk) => sk % 2 === 0;
+// const even = (sk) => !(sk % 2);
+
+// function more1less10(sk) {
+//   return sk > 1 && sk < 10;
+// }
+
+console.log("nums.filter(more1less10) ===", nums.filter(more1less10));
+console.log("nums.filter(even) ===", nums.filter(even));
+
+// const elements = [1, 'true', 'true', false, null, undefined, 'blue', 50];
+
+// atrinkti string tipo el
+
+// atrinkti number tipo el
+
+// atrinkti viska iskyrus string ir number tipo el
--- js/map.js
+++ js/map.js
@@ -6,7 +6,10 @@ console.log("map.js");
 
 // Array.prototype.map - skirtas pakeisti strukturai, grazina nauja masyva, tokio pat ilgio su tiek pat nariu masyva.
 
-const nums = [1, 2, 3];
+// taikomes
+const list1El = document.getElementById("list1");
+
+const nums = [1, -5, 2, 10, -4, 3];
 
 const rez = nums.map((sk, i, arr) => {
   console.log(i, "sk ===", sk);
@@ -19,6 +22,37 @@ const numsObjs = nums.map((sk) => {
   const newObjForArr = { id: sk };
   return newObjForArr;
 });
+// const numsObjs = nums.map((sk) => ({ id: sk }));
 console.log("numsObjs ===", numsObjs);
 
-const numsLiArr = ["<li>1</li>", "<li>2</li>", "<li>3</li>"];
+// const numsLiArr = ['<li>1</li>', '<li>2</li>', '<li>3</li>'];
+const numsLiArrFromMap = nums.map((sk) => {
+  const liHtmlString = `<li>${sk}</li>`;
+  return liHtmlString;
+});
+// [ '<li>1</li>', '<li>2</li>', '<li>3</li>' ]
+console.log("numsLiArrFromMap ===", numsLiArrFromMap);
+{
+  /* 
+<li>1</li>
+<li>2</li>
+<li>3</li> 
+*/
+}
+const stringLis = numsLiArrFromMap.join("");
+console.log(stringLis);
+// list1El.innerHTML = stringLis;
+
+// chain reaction
+list1El.innerHTML = nums.map((sk) => `<li> skaicius ${sk}</li>`).join("");
+
+// grazinti nauja masyva kuriam yra visos nums masyvo reiksmes pakeltos kvadratu
+// sk ** 2
+// const pakelta2 = nums.map((sk) => sk ** 2);
+// console.log('pakelta2 ===', pakelta2);
+
+// tik teigiamos reiksmes
+// list1El.innerHTML = nums
+//   .filter((sk) => sk > 0)
+//   .map((sk) => `<li> skaicius ${sk}</li>`)
+//   .join('');
